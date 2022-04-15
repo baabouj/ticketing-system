fd8ea7e22460becf9fc8ba7e967022e42032d6e9
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-15 14:04:31 +0000

    :heavy_plus_sign: NEW: create database models

--- backend/database/migrations/2022_04_15_135830_create_tickets_table.php
+++ backend/database/migrations/2022_04_15_135830_create_tickets_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('role')->default("user");
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("subject");
+            $table->text("content");
+            $table->foreignId("status_id");
+            $table->foreignId("user_id");
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
