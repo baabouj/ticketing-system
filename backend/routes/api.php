a29113aa75df311a5b344f587bc1e8ac9c237cc8
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-15 15:43:13 +0000

    :lock: NEW: implement auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,17 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
+
+        Route::post("/login", "login");
+
+        Route::post("/signup", "signup");
+
+        Route::get("/logout", "logout")->middleware('auth:sanctum');
+    }
+    );
+
+});
+
+Route::fallback(function () {
+    return response([
+        "message" => "Not Found",
+    ], 404);
 });
