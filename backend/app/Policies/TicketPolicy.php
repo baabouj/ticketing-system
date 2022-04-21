12ec24f1bc64de10845ccce24c2e4f814f9cb476
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-21 15:29:15 +0000

    :heavy_plus_sign: NEW: create ticket policy

---
+++ backend/app/Policies/TicketPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TicketPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * @param User $user
+     * @param Ticket $ticket
+     * @return bool
+     */
+    public function isOwner(User $user, Ticket $ticket): bool
+    {
+        return $user->id === $ticket->user_id;
+    }
+}
