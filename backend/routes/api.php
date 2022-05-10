a82361cbbd0844296f2ff01e06d5b85bb104f59d
Omar Baabouj <baabouj.dev@gmail.com>
2022-05-10 10:56:52 +0100

    :heavy_plus_sign: NEW: add comments crud api endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,46 +1,75 @@
 <?php
 
-use App\Models\User;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('/user', function (Request $request): User {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::prefix('users')->group(function () {
-        Route::controller(\App\Http\Controllers\UserController::class)->group(function () {
-            Route::get("/", "index");
-            Route::get("/{user}", "show");
-            Route::delete("/{user}", "destroy");
-        }
-        );
-    });
-});
-
-Route::prefix('auth')->group(function () {
-    Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
-        Route::post("/login", "login");
-        Route::post("/signup", "signup");
-        Route::post("/logout", "logout")->middleware('auth:sanctum');
-    }
-    );
-});
-
-Route::prefix('tickets')->group(function () {
-    Route::controller(\App\Http\Controllers\TicketController::class)->group(function () {
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::get("/", "index");
-            Route::post("/", "store");
-            Route::get("/{ticket}", "show");
-            Route::put("/{ticket}", "update");
-            Route::delete("/{ticket}", "destroy");
+    Route
+        ::prefix('users')->group(function () {
+            Route
+                ::controller(UserController::class)->group(function () {
+                    Route::get("/", "index");
+                    Route::get("/{user}", "show");
+                    Route::delete("/{user}", "destroy");
+                    Route::get('/{user}/tickets', "tickets");
+                }
+                );
         });
-    }
-    );
 });
+
+Route
+    ::prefix('auth')->group(function () {
+        Route
+            ::controller(AuthController::class)->group(function () {
+                Route::post("/login", "login");
+                Route::post("/signup", "signup");
+                Route::post("/logout", "logout")->middleware('auth:sanctum');
+            }
+            );
+    });
+
+Route
+    ::prefix('tickets')->group(function () {
+        Route
+            ::middleware('auth:sanctum')->group(function () {
+                Route
+                    ::controller(TicketController::class)->group(function () {
+                        Route::get("/", "index");
+                        Route::post("/", "store");
+                        Route::get("/{ticket}", "show");
+                        Route::put("/{ticket}", "update");
+                        Route::delete("/{ticket}", "destroy");
+                    });
+
+                Route
+                    ::controller(CommentController::class)->group(function () {
+                        Route::get("/{ticket}/comments", "index");
+                        Route::post("/{ticket}/comments", "store");
+                    });
+            });
+    });
+
+Route
+    ::prefix('comments')->group(function () {
+        Route
+            ::middleware('auth:sanctum')->group(function () {
+                Route
+                    ::controller(CommentController::class)->group(function () {
+                        Route::get("/{comment}", "show");
+                        Route::put("/{comment}", "update");
+                        Route::delete("/{comment}", "destroy");
+                    });
+            });
+    });
 
 Route::fallback(function () {
     return response([
